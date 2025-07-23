3974c549a8776f754ffbc040db3801ffaf1b6bd5
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-07-23 15:06:00 +0300

    Add farmers routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,7 +3,15 @@ import { Home } from "../pages/Home";
 import { About } from "../pages/About";
 import Shop from '../pages/Shop';
 import Cart from '../pages/Cart';
-import Authmodal from '../components/Authmodal'
+import Authmodal from '../components/Authmodal';
+import ProfileView from '../pages/Farmers/ProfileView';
+import ProfileEdit from '../pages/Farmers/ProfileEdit';
+import AnimalList from '../pages/Farmers/AnimalList';
+import AnimalForm from '../pages/Farmers/AnimalForm';
+import FarmerFeedback from '../pages/Farmers/FarmerFeedback';
+import FarmerLogin from "../pages/Farmers/FarmerLogin";
+import FarmerRegister from "../pages/Farmers/FarmerRegister";
+
 
 const appRoutes = [
   { path: "/", element: <Home /> },
@@ -11,7 +19,14 @@ const appRoutes = [
   {path: '/shop', element:<Shop/>},
   {path:'/cart', element:<Cart/>},
   {path:'/Signin', element:<Authmodal/>},
-  {path:'Signin', element:<Authmodal/>}
+  {path:'Signin', element:<Authmodal/>},
+  {path: "/farmers/profile",element: <ProfileView />},
+  {path: "/farmers/edit",element: <ProfileEdit />},
+  {path: "/farmers/animals",element: <AnimalList />},
+  {path: "/farmers/animals/new",element: <AnimalForm />},
+  {path: "/farmers/feedback",element: <FarmerFeedback />},
+  {path: "/farmers/login",element: <FarmerLogin />},
+  {path: "/farmers/register",element: <FarmerRegister />},
 ];
 
 export default appRoutes;
