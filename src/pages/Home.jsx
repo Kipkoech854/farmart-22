6a494df3234c7857eaea70282d08c75aa23c1bf4
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-07-24 21:28:45 +0300

    Make it responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,40 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import '../Stylesheets/Home.css'
+import React from 'react';
+import '../Stylesheets/Home.css';
+import { Link } from 'react-router-dom';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
 
-export const Home =() =>{
+export const Home = () => {
+
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: true });
+  }, []);
+
+
   return (
-    <div className='home-container'>
-      <div className='welcome'>
-         <h1>Welcome to farmart</h1>
-      </div> 
-
-    <div className='content'>
-      <div className='bridge'>
-        <p>Bridging the gap between farmers and Buyers</p>
-        <p>Seamlessly in farm spaces</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  
-          tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim  veniam, 
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat</p>
+    <div className="landing-container">
+      <div className="text-section" data-aos="fade-right">
+        <p className="bridge-line">
+          <span className="highlight">Bridging</span> the gap between farmers and buyers
+        </p>
+        <h1>
+          Seamlessly in <span className="highlight">Farm</span> Spaces
+        </h1>
+        <p className="description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+          nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <div className="button-group" data-aos="fade-up">
+          <Link to="/signup" className="btn primary">Get Started</Link>
+          <Link to="/shop" className="btn outline">Our Services</Link>
+        </div>
       </div>
 
-      <div className='image-container'>
-        <img src='./images/Landing-horse.png' alt='landing-horse' className=''/>
+      <div className="image-section" data-aos="fade-left">
+        <img src="./images/Landing-horse.png" alt="Horse" />
       </div>
     </div>
-
-      <div className='nav-links'>
-        
-        <nav>
-          <Link to='/signup'>Get started</Link>{'     '}
-          <Link to ='/shop'>Our Services</Link>
-        </nav>  
-      </div>
-
-    </div>
-
-  )
-}
-
-
+  );
+};
