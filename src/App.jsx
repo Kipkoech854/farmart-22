00420aa621cf97565261a03e5d8409d6a2171d06
chiscobyte <chiscoenock@gmail.com>
2025-07-25 16:38:45 +0300

    pulling from main

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+
 import appRoutes from "./Routes/Routes";
-<<<<<<< HEAD
-import { CartProvider } from "./context/CartContext"; 
-=======
+
+import { CartProvider } from "./context/CartContext";
+
 import Footer from "./components/Footer";
->>>>>>> daf0b8a205dd01322067b6ba427879a126f3541a
+
 
 function App() {
   return (
-    <CartProvider>
-      <Navbar />
-      <Routes>
-        {appRoutes.map(({ path, element }, index) => (
-          <Route key={index} path={path} element={element} />
-        ))}
-      </Routes>
-<<<<<<< HEAD
-    </CartProvider>
-=======
+    <>
+      <CartProvider>
+        <Navbar />
+        <Routes>
+          {appRoutes.map(({ path, element }, index) => (
+            <Route key={index} path={path} element={element} />
+          ))}
+        </Routes>
+      </CartProvider>
+
       <Footer />
     </>
->>>>>>> daf0b8a205dd01322067b6ba427879a126f3541a
+
   );
 }
 
