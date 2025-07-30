7efa7160852ec833bc5114757b6a1f743fc43540
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-07-30 15:29:14 +0300

    fix: update API endpoint in ProfileView component to use production URL

--- src/pages/Farmers/ProfileView.jsx
+++ src/pages/Farmers/ProfileView.jsx
@@ -8,7 +8,7 @@ const ProfileView = () => {
 
   useEffect(() => {
     const token = localStorage.getItem("token");
-    fetch("http://localhost:5555/api/farmers/farmers", {
+    fetch("https://farmart-y80m.onrender.com/api/farmers/farmers", {
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -20,22 +20,24 @@ const ProfileView = () => {
 
   return (
     <div>
-      <FarmerNavbar />
-      <div className="profile-container">
-        <h2 className="profile-title">My Profile</h2>
-        {farmer ? (
-          <div className="profile-card">
-            <img className="profile-pic"src={farmer.profile_picture? `http://localhost:5555/${farmer.profile_picture}` : defaultPic}
-               alt="Profile"
-             />
+      <div className="page-wrapper">  
+        <FarmerNavbar />
+        <div className="profile-container">
+          <h2 className="profile-title">My Profile</h2>
+          {farmer ? (
+            <div className="profile-card">
+              <img className="profile-pic"src={farmer.profile_picture? `https://farmart-y80m.onrender.com/${farmer.profile_picture}` : defaultPic}
+                alt="Profile"
+              />
 
-            <p><strong>Username:</strong> {farmer.username}</p>
-            <p><strong>Email:</strong> {farmer.email}</p>
-            <p><strong>Phone:</strong> {farmer.phone || "N/A"}</p>
-          </div>
-        ) : (
-          <p>Loading...</p>
-        )}
+              <p><strong>Username:</strong> {farmer.username}</p>
+              <p><strong>Email:</strong> {farmer.email}</p>
+              <p><strong>Phone:</strong> {farmer.phone || "N/A"}</p>
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
       </div>
     </div>
   );
